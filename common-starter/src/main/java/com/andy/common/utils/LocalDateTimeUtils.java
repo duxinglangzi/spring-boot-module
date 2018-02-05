bc70cb2fcc36abb15ceab4b71e8fed15d37482e3
duxinglangzi <871364441@qq.com>
2018-02-05 10:14:49 +0800

    增加 date 转换 localDateTime

--- common-starter/src/main/java/com/andy/common/utils/LocalDateTimeUtils.java
+++ common-starter/src/main/java/com/andy/common/utils/LocalDateTimeUtils.java
@@ -10,7 +10,8 @@ import java.util.Date;
  */
 public class LocalDateTimeUtils {
 
-    public static final ZoneId zoneId = ZoneId.systemDefault();
+    /** 使用系统默认时区 */
+    public static final ZoneId ZONE_ID = ZoneId.systemDefault();
 
     /**
      * 方法描述: 将java.time.LocalDate 转化为 java.util.Date
@@ -19,7 +20,7 @@ public class LocalDateTimeUtils {
      * @author wuqiong  2018/1/29 11:17
      */
     public static Date localDateToDate(final LocalDate localDate){
-        ZonedDateTime zonedDateTime = localDate.atStartOfDay(zoneId);//使用系统默认时区
+        ZonedDateTime zonedDateTime = localDate.atStartOfDay(ZONE_ID);
         return Date.from(zonedDateTime.toInstant());
     }
 
@@ -30,19 +31,29 @@ public class LocalDateTimeUtils {
      * @author wuqiong  2018/1/29 11:21
      */
     public static Date localDateTimeToDate(final LocalDateTime localDateTime){
-        ZonedDateTime zonedDateTime = localDateTime.atZone(zoneId);//使用系统默认时区
+        ZonedDateTime zonedDateTime = localDateTime.atZone(ZONE_ID);
         return Date.from(zonedDateTime.toInstant());
+    }
+
+    /**
+     * 方法描述: 将java.utils.Date 转化为 java.time.LocalDateTime
+     * @params date
+     * @return LocalDateTime
+     * @author wuqiong  2018/2/5 10:08
+     */
+    public static LocalDateTime dateToLocalDateTime(final Date date){
+        return LocalDateTime.ofInstant(date.toInstant(), ZONE_ID);
     }
 
     /**
      * 方法描述: 获取两个时间差
      * @params LocalDateTime beginTime,LocalDateTime endTime
      * @return Duration 返回一个持续时间
-     *  <p>toNanos()//纳秒</p>
-     *  <p>toMillis()//毫秒</p>
-     *  <p>toMinutes()//分钟</p>
-     *  <p>toHours()//小时</p>
-     *  <p>toDays()//天数</p>
+     *  <p>Duration.toNanos()//纳秒</p>
+     *  <p>Duration.toMillis()//毫秒</p>
+     *  <p>Duration.toMinutes()//分钟</p>
+     *  <p>Duration.toHours()//小时</p>
+     *  <p>Duration.toDays()//天数</p>
      * @author wuqiong  2018/1/29 13:05
      */
     public static Duration localBetween(final LocalDateTime beginTime, final LocalDateTime endTime){
