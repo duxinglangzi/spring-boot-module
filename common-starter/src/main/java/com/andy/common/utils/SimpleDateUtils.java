14fc26b0311a0b6bd4eb009cc2583d19c76ef84a
duxinglangzi <871364441@qq.com>
2018-02-05 10:05:17 +0800

    修改命名方式

--- common-starter/src/main/java/com/andy/common/utils/SimpleDateUtils.java
+++ common-starter/src/main/java/com/andy/common/utils/SimpleDateUtils.java
@@ -1,5 +1,6 @@
 package com.andy.common.utils;
 
+import org.springframework.util.ObjectUtils;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -7,41 +8,68 @@ import java.util.Date;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-
 /**
  * <p>Description: 实现java日期格式化 </p>
  * @author wuqiong  2017年8月31日
  */
 public final class SimpleDateUtils {
 
-    private static final ConcurrentMap<Patten,ThreadLocal<SimpleDateFormat>> hashMap;//日期模板集合
-
-    /**
-     * <pre> SimpleDateUtils.to_DATE_ONLY_FORMAT(new Date())="2017-09-01" </pre>
-     */
-    public static String to_DATE_ONLY_FORMAT(Date params) {
-        CheckParamsIsEmpty(params);
-        return hashMap.get(Patten.DATE_ONLY_FORMAT).get().format(params);
-    }
-
-    public static Date parse_DATE_ONLY_FORMAT(String params) throws ParseException {
-        CheckParamsIsEmpty(params);
-        return hashMap.get(Patten.DATE_ONLY_FORMAT).get().parse(params);
-    }
+    /** 日期模板集合 */
+    private static final ConcurrentMap<Patten,ThreadLocal<SimpleDateFormat>> PATTEN_THREAD_LOCAL_CONCURRENT_MAP;
 
     /**
      * <pre> SimpleDateUtils.to_DATE_ONLY_FORMAT(new Date())="2017-09-01 12:35:30" </pre>
      */
     public static String to_DATE_TIME_FORMAT(Date params) {
         CheckParamsIsEmpty(params);
-        return hashMap.get(Patten.DATE_TIME_FORMAT).get().format(params);
+        return PATTEN_THREAD_LOCAL_CONCURRENT_MAP.get(Patten.DATE_TIME_FORMAT).get().format(params);
+    }
+    /**
+     *  SimpleDateUtils.parse_DATE_TIME_FORMAT("2017-09-01 12:35:30")=new Date()
+     */
+    public static Date parse_DATE_TIME_FORMAT(String params) throws ParseException{
+        CheckParamsIsEmpty(params);
+        return PATTEN_THREAD_LOCAL_CONCURRENT_MAP.get(Patten.DATE_TIME_FORMAT).get().parse(params);
     }
 
 
+    /**
+     * <pre> SimpleDateUtils.to_DATE_ONLY_FORMAT(new Date())="2017-09-01" </pre>
+     */
+    public static String to_DATE_ONLY_FORMAT(Date params) {
+        CheckParamsIsEmpty(params);
+        return PATTEN_THREAD_LOCAL_CONCURRENT_MAP.get(Patten.DATE_ONLY_FORMAT).get().format(params);
+    }
+    /**
+     * <pre> SimpleDateUtils.parse_DATE_ONLY_FORMAT("2017-09-01")=new Date() </pre>
+     */
+    public static Date parse_DATE_ONLY_FORMAT(String params) throws ParseException {
+        CheckParamsIsEmpty(params);
+        return PATTEN_THREAD_LOCAL_CONCURRENT_MAP.get(Patten.DATE_ONLY_FORMAT).get().parse(params);
+    }
+
+
+    /**
+     * <pre> SimpleDateUtils.to_DATE_ONLY_FORMAT(new Date())="15:35:23" </pre>
+     */
+    public static String to_TIME_FORMAT(Date params) {
+        CheckParamsIsEmpty(params);
+        return PATTEN_THREAD_LOCAL_CONCURRENT_MAP.get(Patten.TIME_FORMAT).get().format(params);
+    }
+    /**
+     * <pre> SimpleDateUtils.parse_DATE_ONLY_FORMAT("15:35:23")=new Date() </pre>
+     */
+    public static Date parse_TIME_FORMAT(String params) throws ParseException {
+        CheckParamsIsEmpty(params);
+        return PATTEN_THREAD_LOCAL_CONCURRENT_MAP.get(Patten.TIME_FORMAT).get().parse(params);
+    }
+
+
+
     static{
-        hashMap=new ConcurrentHashMap<Patten, ThreadLocal<SimpleDateFormat>>();
+        PATTEN_THREAD_LOCAL_CONCURRENT_MAP=new ConcurrentHashMap<Patten, ThreadLocal<SimpleDateFormat>>();
         for (final Patten patten : Patten.values()) {
-            hashMap.put(patten, new ThreadLocal<SimpleDateFormat>() {
+            PATTEN_THREAD_LOCAL_CONCURRENT_MAP.put(patten, new ThreadLocal<SimpleDateFormat>() {
                 @Override
                 public SimpleDateFormat initialValue() {
                     return new SimpleDateFormat(patten.getPatten());
@@ -56,8 +84,9 @@ public final class SimpleDateUtils {
      * @author wuqiong 2017年8月31日 下午1:46:50
      */
     private static void CheckParamsIsEmpty(Object obj) {
-        if (ObjectUtils.isEmpty(obj))
+        if (ObjectUtils.isEmpty(obj)){
             throw new IllegalArgumentException("The params must not be null");
+        }
     }
 
 
@@ -67,14 +96,14 @@ public final class SimpleDateUtils {
      * @author wuqiong  2017年8月31日
      */
     private enum Patten{
-        /** 格式：年－月－日 **/
-        DATE_ONLY_FORMAT("yyyy-MM-dd"),
+        /** 格式：年－月－日 小时：分钟：秒 **/
+        DATE_TIME_FORMAT("yyyy-MM-dd HH:mm:ss"),
 
         /** 格式：年－月－日 小时：分钟 **/
         DATE_HOUR_MINUTE_FORMAT("yyyy-MM-dd HH:mm"),
 
-        /** 格式：年－月－日 小时：分钟：秒 **/
-        DATE_TIME_FORMAT("yyyy-MM-dd HH:mm:ss"),
+        /** 格式：年－月－日 **/
+        DATE_ONLY_FORMAT("yyyy-MM-dd"),
 
         /** 格式：月－日 **/
         MONTH_DAY_FORMAT("MM-dd"),
